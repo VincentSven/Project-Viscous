f6c668ca7b1f85ee8f1b1554f4e91181e401cc21
Vincent Sven <djvinzo99@gmail.com>
2016-09-08 20:48:12 +0200

    Revert "Revert "Viscous began""
    This reverts commit db31a1de2e46238572dd7aaef90a5ad370cf5c9c.

---
+++ Viscous/index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<html>
+	<head>
+		
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="head">
+				head
+			</div>
+			<div id="menu">
+				menu
+			</div>
+			<div id="content">
+				content
+			</div>		
+		</div>
+		<div id="footer">
+			footer
+		</div>
+	</body>
+</html>
