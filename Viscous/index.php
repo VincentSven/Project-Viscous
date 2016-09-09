acc0b268576bd3d2c577a536afa17c967c741a30
Vincent Sven <djvinzo99@gmail.com>
2016-09-09 19:32:44 +0200

    changed navbar

--- Viscous/index.php
+++ Viscous/index.php
@@ -4,7 +4,6 @@
 		<link href="styles/navbar.css" type="text/css" rel="stylesheet"/>
 	</head>
 	<body>
-		<div id="test"></div>
 		<div id="wrapper">
 			<div id="header">
 				<h1> Project Viscous</h1>
