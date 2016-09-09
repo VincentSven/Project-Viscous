d7328b2739db4c42a585e961e220378b42951049
Vincent Sven <djvinzo99@gmail.com>
2016-09-09 18:58:19 +0200

    Centered webpage, added navbar (markup only)

--- Viscous/index.php
+++ Viscous/index.php
@@ -1,14 +1,21 @@
 <html>
 	<head>
-		<link href="styles/style.css" />
+		<link href="styles/style.css" type="text/css" rel="stylesheet"/>
+		<link href="styles/navbar.css" type="text/css" rel="stylesheet"/>
 	</head>
 	<body>
 		<div id="wrapper">
 			<div id="header">
-				head
+				<h1> Project Viscous</h1>
 			</div>
 			<div id="menu">
-				menu
+				<ol>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">inloggen</a></li>
+					<li><a href="#">info</a></li>
+					<li><a href="#">forum</a></li>
+					<li><a href="#">spelregels</a></li>
+				</ol>
 			</div>
 			<div id="content">
 				content
