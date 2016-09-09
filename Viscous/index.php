0000586563d6aa89741850903111ee9f96d33ab6
Vincent Sven <djvinzo99@gmail.com>
2016-09-09 19:19:20 +0200

    ol to ul

--- Viscous/index.php
+++ Viscous/index.php
@@ -9,13 +9,13 @@
 				<h1> Project Viscous</h1>
 			</div>
 			<div id="menu">
-				<ol>
+				<ul>
 					<li><a href="#">Home</a></li>
 					<li><a href="#">inloggen</a></li>
 					<li><a href="#">info</a></li>
 					<li><a href="#">forum</a></li>
 					<li><a href="#">spelregels</a></li>
-				</ol>
+				</ul>
 			</div>
 			<div id="content">
 				content
