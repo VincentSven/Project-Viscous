809e876eba55e84527af69824e4b2a8574fd8a65
Vincent Sven <djvinzo99@gmail.com>
2016-11-09 20:19:25 +0100

    MySQLi Connection
    Connection with the database

--- Viscous/config/config.php
+++ Viscous/config/config.php
@@ -9,8 +9,21 @@
 	$seperator 					= ' - ';
 	$posttitle 					= 'Lands';
 	$title 						= $pretitle . $seperator . $posttitle;
+	
+	//Database variables
+	$dbserver					= 'localhost';
+	$dbusername					= 'root';
+	$dbpassword					= '';
+	$db							= 'solalands';
 
-
-
+	//DATABASE CONNECTION
+	$conn						= new mysqli($dbserver, $dbusername, $dbpassword);
+	
+	//DATABASE CONNECTION CHECK
+	if ($conn->connect_error){
+		die('Database connection lost!' . $conn->connect_error);
+	}else{
+		echo 'Succesvol geconnect!';
+	};
 
 ?>
