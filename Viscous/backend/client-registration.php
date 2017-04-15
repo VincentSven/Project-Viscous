61d12d71759651fa0dc2345876bf328ba6196f47
Vincent Sven <djvinzo99@gmail.com>
2017-04-15 23:29:26 +0200

    Begin select protection
    in client registration started with anti sql-injecting select, not done
    yet

--- Viscous/backend/client-registration.php
+++ Viscous/backend/client-registration.php
@@ -15,8 +15,13 @@
 	
 	//PREPARING STATEMENTS
 	$stmt_ins = $conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
-	$stmt_ins->bind_param("sss", $username, $password, $email);
+	$stmt_ins->bind_param("sss", $username, $password, $mail);
 	
+	$stmt_select_uname = $conn->prepare("SELECT username FROM users WHERE username = ?");
+	$stmt_select_uname->bind_param("s", $username);
+	
+	$stmt_select_mail = $conn->prepare("SELECT email FROM users WHERE email = ?");
+	$stmt_select_mail->bind_param("s", $mail);
 	
 	//ALLOCATIONS
 	$mail		=	$_POST['mail'];
@@ -29,14 +34,14 @@
 	//TEST FOR HACKING
 	
 	//CHECK UNIQUENESS OF THA VALUES
-	$sql_uname 	= "SELECT username FROM users WHERE username = '$username'";
-	if ($result=mysqli_query($conn,$sql_uname)) 
+	$stmt_select_uname->execute();
+	if ($result= mysqli_query($conn,$stmt_select_uname)) 
 	{
 		$rowcount_uname = $result->num_rows;
 	}
 	
-	$sql_uname 	= "SELECT email FROM users WHERE email = '$mail'";	
-	if ($result_e=mysqli_query($conn, $sql_uname)) {
+	$stmt_select_mail->execute();
+	if ($result_e= $stmt_select_mail) {
 		$rowcount_mail = $result_e->num_rows;
 	}
 	
@@ -50,12 +55,17 @@
 	}
 	else
 	{
+			$stmt_select_mail->close();
+			$stmt_select_uname->close();
 			
 			//EXECUTE QUERY
 			if ($stmt_ins->execute()	===	TRUE) {
 				echo 'Account succesvol toegevoegd aan de database!';
 			} else {
-				echo 'ERROR'.$stmt_ins.'</br>'.$conn->error;
+				$stmt_ins->store_result();
+				$stmt_ins->bind_result($username, $password, $email);
+				$stmt_ins->fetch();
+				echo 'ERROR'.$users.$password.$email.'</br>'.$conn->error;
 			}
 	}
 	
