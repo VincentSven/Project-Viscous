dc64cea8c9832914d0a8cc311a7cdb727f212102
Vincent Sven <djvinzo99@gmail.com>
2017-04-16 17:48:29 +0200

    Registration sql inject proof
    finally

--- Viscous/backend/client-registration.php
+++ Viscous/backend/client-registration.php
@@ -16,12 +16,8 @@
 	//PREPARING STATEMENTS
 	$stmt_ins = $conn->prepare("INSERT INTO users (username, password, email) VALUES (?, ?, ?)");
 	$stmt_ins->bind_param("sss", $username, $password, $mail);
-	
-	$stmt_select_uname = $conn->prepare("SELECT username FROM users WHERE username = ?");
-	$stmt_select_uname->bind_param("s", $username);
-	
-	$stmt_select_mail = $conn->prepare("SELECT email FROM users WHERE email = ?");
-	$stmt_select_mail->bind_param("s", $mail);
+
+
 	
 	//ALLOCATIONS
 	$mail		=	$_POST['mail'];
@@ -34,14 +30,20 @@
 	//TEST FOR HACKING
 	
 	//CHECK UNIQUENESS OF THA VALUES
-	$stmt_select_uname->execute();
-	if ($result= mysqli_query($conn,$stmt_select_uname)) 
+	$stmt_uname = $conn->prepare("SELECT username FROM users WHERE username = ?");
+	$stmt_uname->bind_param("s", $username);
+	$stmt_uname->execute();
+	$stmt_uname->store_result();
+	if ($result= $stmt_uname) 
 	{
 		$rowcount_uname = $result->num_rows;
 	}
-	
-	$stmt_select_mail->execute();
-	if ($result_e= $stmt_select_mail) {
+
+	$stmt_mail = $conn->prepare("SELECT email FROM users WHERE email = ?");
+	$stmt_mail->bind_param("s", $mail);
+	$stmt_mail->execute();
+	$stmt_mail->store_result();
+	if ($result_e = $stmt_mail) {
 		$rowcount_mail = $result_e->num_rows;
 	}
 	
@@ -55,20 +57,17 @@
 	}
 	else
 	{
-			$stmt_select_mail->close();
-			$stmt_select_uname->close();
 			
 			//EXECUTE QUERY
 			if ($stmt_ins->execute()	===	TRUE) {
 				echo 'Account succesvol toegevoegd aan de database!';
 			} else {
-				$stmt_ins->store_result();
-				$stmt_ins->bind_result($username, $password, $email);
-				$stmt_ins->fetch();
-				echo 'ERROR'.$users.$password.$email.'</br>'.$conn->error;
+				echo 'ERROR'.$stmt_ins.'</br>'.$conn->error;
 			}
 	}
 	
+	$stmt_uname->close();
+	$stmt_mail->close();
 	$stmt_ins->close();
 	$conn->close();
 ?>
