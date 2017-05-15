803a053b57dc5a260c31c391f8358c57af9ed2cc
gijswl <gijswlagerweij@gmail.com>
2017-05-15 18:19:09 +0200

    Updated News system:  - Loads news after the page has loaded (AJAX)  - Caches news in news.xml  - Not dependent on msg_index table anymore  - Automatically sorts news by date

--- Viscous/frontend/pages/index.php
+++ Viscous/frontend/pages/index.php
@@ -5,22 +5,47 @@
 <html lang="nl">
 	<head>
 		<?php require_once('Frontend/templates/head.php');?>
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script type="text/javascript">
+        	function loadNews() {
+           		var newsfeed = document.getElementById("newsfeed");
+           		
+           		jQuery.ajax({ 
+           			type: "POST",
+           			url: "system/news.php",
+           			dataType: 'json',
+           			data: { functionname: 'getNews' },
+           			
+           			success: function(obj, status){
+           				if( !('error' in obj) ) {
+                      		newsfeed.innerHTML = obj.news;
+                  		} else {
+                      		console.log(obj.error);
+                  		}
+           			}
+           		});
+           	}
+        	window.onload = loadNews;
+       	</script>
 	</head>
 	<body>
 		<?php require_once('Frontend/templates/header.php') ;?>
 		<div id="wrapper">
 			<div class="spacer"></div>
 			<div id="content">
-				<div class="content_left">
+				<div class="content_left" id="content_left">
 					<?php 
 						getMsg();
 						getGlink();
 					?>
 				</div>
-				<div class="content_right">
+				<div class="content_right" id="content_right">
 					<!--<p>Hier alle updates graag! Kan iemand er een update register even in PHP'en? Danku!</p>-->
 					<h2>News feed</h2>
-					<?php require_once('system/news.php'); ?>
+					<div id="newsfeed">
+						
+					</div>
 				</div>
 			</div>
 		</div>
--- Viscous/system/news.php
+++ Viscous/system/news.php
@@ -1,34 +1,85 @@
 <?php
-	global $dbserver;
-	global $dbusername;
-	global $dbpassword;
-	global $max_news;
-	
-	$db = "sola_news";
-	
-	$conn = new mysqli($dbserver, $dbusername, $dbpassword, $db);
-	if ($conn->connect_error){
-		die('Database connection lost! ' . $conn->connect_error);
-	}else{
-		$query = "SELECT `id` FROM msg_index WHERE 1";
-		$result = mysqli_query($conn, $query);
+	function updateNews(){
+		global $dbserver;
+		global $dbusername;
+		global $dbpassword;
+		global $max_news;
 		
-		$news = [];
-		
-		//TODO? this relies on the 'msg_index' table to only contain the $max_news latest messages in ascending chronological order
-		// Preferably not do this at load time, but use AJAX? to nicely sort everything after the page loaded
-		$count = mysqli_num_rows($result);
-		for ($i=0; $i < min($count, $max_news); $i++) {
-			$row = mysqli_fetch_assoc($result); 
-			$query = "SELECT `text`, `title`, `time` FROM news_messages WHERE `id`=" . $row['id'] . "";
+		$conn = new mysqli($dbserver, $dbusername, $dbpassword, "sola_news");
+		if ($conn->connect_error){
 			
-			$msg = mysqli_query($conn, $query);
-			$msg_row = mysqli_fetch_assoc($msg);
-			$news[$i] = "<b>" . $msg_row['title'] . "</b><br/><i>" . $msg_row['time'] . "</i><br/>" . $msg_row['text'] . "<br/><br/>"; //TODO styling
-		}
+		}else{
+			$query = "SELECT `id`, `time` FROM news_messages WHERE 1";
+			$result = mysqli_query($conn, $query);
 		
-		for ($i=min($count, $max_news); $i > 0; $i--) { // Reverses order to be descending chronologic
-			echo $news[$i - 1];
+			$news = [];
+			$time = [];
+			
+			$count = mysqli_num_rows($result);
+			for ($i=0; $i < $count; $i++) {
+				$row = mysqli_fetch_assoc($result); 
+				$news[$i] = $row['id'];
+				$time[$i] = $row['time'];
+			}
+			array_multisort($time, SORT_DESC, $news);
+			
+			$dom = new DomDocument('1.0', 'UTF-8'); 
+	    	$root = $dom->appendChild($dom->createElement('news'));
+			for ($i=0; $i < min($count, $max_news); $i++) {
+				$query = "SELECT `text`, `title` FROM news_messages WHERE `id`=" . $news[$i] . "";
+				$res = mysqli_query($conn, $query);
+				$msg_row = mysqli_fetch_assoc($res);
+				
+	    		$msg = $dom->createElement('message');
+	    		$root->appendChild($msg);
+				
+				$msg_title = $dom->createAttribute('title');
+    			$msg_title->appendChild($dom->createTextNode($msg_row['title']));
+    			$msg->appendChild($msg_title);
+				
+				$msg_text = $dom->createAttribute('text');
+				$msg_text->appendChild($dom->createTextNode($msg_row['text']));
+    			$msg->appendChild($msg_text);
+				
+				$msg_date = $dom->createAttribute('date');
+				$msg_date->appendChild($dom->createTextNode($time[$i]));
+    			$msg->appendChild($msg_date);
+			}
+	
+	    	$dom->formatOutput = true; // Set to false to save a little bit of space
+	    	$dom->save('news.xml');
 		}
 	}
+
+	function getNews(){
+		$xml = simplexml_load_file('http://localhost/phpwebgame/viscous/news.xml');
+		
+		$total = "";
+		foreach($xml->message as $msg){
+			$title = $msg['title'][0];
+			$text = $msg['text'][0];
+			$date = $msg['date'][0];
+			
+			$text = "{$title}<br/>{$date}<br/>{$text}<br/><br/>";
+			$total .= $text;
+		}
+		return $total;
+	}
+	
+	header('Content-Type: application/json');
+	$result = [];
+	
+	if( !isset($_POST['functionname']) ) { $result['error'] = 'Invalid POST request'; }
+	if( !isset($result['error']) ) {
+        switch($_POST['functionname']) {
+			case 'getNews':
+				$result['news'] = getNews();
+				break;
+			default:
+				$result['error'] = 'Invalid POST request';
+				break;
+		}
+	}
+	
+	echo json_encode($result);
 ?>
