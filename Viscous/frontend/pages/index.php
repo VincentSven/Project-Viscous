42c38dd00090ed3097f08fc093dec704549d4d51
gijswl <gijswlagerweij@gmail.com>
2017-05-17 11:32:58 +0200

    Fixed content displaying over footer TODO: Scrolling doesnt work because #page is under footer

--- Viscous/frontend/pages/index.php
+++ Viscous/frontend/pages/index.php
@@ -34,20 +34,22 @@
 	</head>
 	<body>
 		<?php require_once('frontend/templates/header.php') ;?>
-		<div id="wrapper">
-			<div class="spacer"></div>
-			<div id="content">
-				<div class="content_left">
-					<?php 
-						getMsg();
-						getGlink();
-					?>
-				</div>
-				<div class="content_right">
-					<!--<p>Hier alle updates graag! Kan iemand er een update register even in PHP'en? Danku!</p>-->
-					<h2>News feed</h2>
-					<div id="newsfeed">
-						
+		<div id="page">
+			<div id="wrapper">
+				<div class="spacer"></div>
+				<div id="content">
+					<div class="content_left">
+						<?php 
+							getMsg();
+							getGlink();
+						?>
+					</div>
+					<div class="content_right">
+						<!--<p>Hier alle updates graag! Kan iemand er een update register even in PHP'en? Danku!</p>-->
+						<h2>News feed</h2>
+						<div id="newsfeed">
+							
+						</div>
 					</div>
 				</div>
 			</div>
