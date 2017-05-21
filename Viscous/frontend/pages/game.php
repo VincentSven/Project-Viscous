e1254475e577b5544739c1f270526c116c67e81f
gijswl <gijswlagerweij@gmail.com>
2017-05-21 14:17:30 +0200

    Added basic phaser initialization

--- Viscous/frontend/pages/game.php
+++ Viscous/frontend/pages/game.php
@@ -5,16 +5,16 @@
 <html>
 	<head>
 		<?php require_once('frontend/templates/head.php');?>
-		<script src="lib/jquery-3.2.1.min.js" type="text/javascript"></script>
+		<!--<script src="lib/jquery-3.2.1.min.js" type="text/javascript"></script>-->
+		<script src="lib/phaser/v2/build/phaser.min.js" type="text/javascript"></script>
+		<script src="game/main.js" type="application/javascript"></script>
 	</head>
 	<body>
 	<?php require_once('frontend/templates/header.php') ;?>
 		<div id="page">
 			<div id="wrapper">
 				<div class="spacer"></div>
-				<div class="game_content">
-					<canvas id="game_canvas"></canvas>
-				</div>
+				<div class="game_content" id="game_content"></div>
 			</div>
 		</div>
 		<?php require_once 'frontend/templates/header.php'; ?>
---
+++ Viscous/game/main.js
@@ -0,0 +1,13 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "game_content", { preload: preload, create: create, update: update });
+
+function preload(){
+	
+}
+
+function create(){
+	
+}
+
+function update(){
+	
+}
